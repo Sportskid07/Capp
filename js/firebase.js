830664e52ce3a07557120ee1bd849bc99e953ecf
Sportskid07 <crickid18@gmail.com>
2026-01-03 11:32:41 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyCzqMdM4om2ZQawvNSY5ToTfztqYmSJEbs",
+  authDomain: "capp-334e2.firebaseapp.com",
+  projectId: "capp-334e2",
+  storageBucket: "capp-334e2.appspot.com",
+  messagingSenderId: "260888838612",
+  appId: "1:260888838612:web:71b7a609be7e4b5b4e1641"
+};
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCzqMdM4om2ZQawvNSY5ToTfztqYmSJEbs",
-    authDomain: "capp-334e2.firebaseapp.com",
-    projectId: "capp-334e2",
-    storageBucket: "capp-334e2.firebasestorage.app",
-    messagingSenderId: "260888838612",
-    appId: "1:260888838612:web:71b7a609be7e4b5b4e1641"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
