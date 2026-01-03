749cee36970feac11a59433546efceb05c786f56
Sportskid07 <crickid18@gmail.com>
2026-01-03 11:34:52 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,13 +2,43 @@ import { signInWithEmailAndPassword } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { auth } from "./firebase.js";
 
-window.login = function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("loginBtn");
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => window.location.href = "chat.html")
-    .catch(err => alert(err.message));
-};
+  btn.addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        window.location.href = "chat.html";
+      })
+      .catch(err => {
+        alert(err.message);
+        console.error(err);
+      });
+  });
+});
+import { signInWithEmailAndPassword } from 
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { auth } from "./firebase.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("loginBtn");
+
+  btn.addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        window.location.href = "chat.html";
+      })
+      .catch(err => {
+        alert(err.message);
+        console.error(err);
+      });
+  });
+});
 
 
