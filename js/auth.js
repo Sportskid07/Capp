207afa62105daac6441fa61337b778e61bd6fb90
Sportskid07 <crickid18@gmail.com>
2026-01-03 11:23:16 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,17 @@
+import { signInWithEmailAndPassword } from 
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { auth } from "./firebase.js";
+
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "chat.html";
+    })
+    .catch(() => {
+      alert("Access denied");
+    });
+};
 
