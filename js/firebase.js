b494648f3b6afb26fde680e5569d8edfabf3df3b
Sportskid07 <crickid18@gmail.com>
2026-01-03 11:22:27 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCzqMdM4om2ZQawvNSY5ToTfztqYmSJEbs",
+    authDomain: "capp-334e2.firebaseapp.com",
+    projectId: "capp-334e2",
+    storageBucket: "capp-334e2.firebasestorage.app",
+    messagingSenderId: "260888838612",
+    appId: "1:260888838612:web:71b7a609be7e4b5b4e1641"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
